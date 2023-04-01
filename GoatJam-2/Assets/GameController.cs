9f0103311de840c88ab19386debc245653c7940a
gngEren <eren_1234_gungor@hotmail.com>
2023-04-01 18:51:12 +0300

    newscene
    s

---
+++ GoatJam-2/Assets/GameController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameController : MonoBehaviour
+{
+    private static GameController gameController;
+    [SerializeField] private MapBorder Map;
+
+
+    [Serializable]
+    public struct MapBorder
+    {
+        public float maxY;
+        public float minY;
+        public float maxX;
+        public float minX;
+
+    }
+    private void Awake() {
+        gameController=this;
+        
+    }
+    public static float GetMaxX()
+    {
+        return gameController.Map.maxX;
+
+
+    }
+    public static float GetMaxY()
+    {
+        return gameController.Map.maxY;
+
+
+    }
+    public static float GetMinX()
+    {
+        return gameController.Map.minX;
+
+
+    }
+    public static float GetMinY()
+    {
+        return gameController.Map.minY;
+
+
+    }
+
+
+
+
+    
+    
+}
---
+++ GoatJam-2/Assets/PlayerMovement.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private jumpSpeedParameter jump;
+    [Serializable]
+    public struct jumpSpeedParameter
+    {
+        public float speed;
+        public float durationTime; //zıplama halinde geçen süresi;
+        public float hangTime;// zıpladıktan sonra bekleme süresi;
+
+    }
+
+    private float jumpRelaodTime;  //zıplama halinde geçen süresi + zıpladıktan sonra bekleme süresi
+    private Rigidbody2D rb;
+    private Vector2 rightDirection=Vector2.right;
+    private Vector2 leftDirection=Vector2.left;
+    private Vector2 upDirection=Vector2.up;
+    private Vector2 downDirection=Vector2.down;
+    private Vector2 origineDot=Vector2.down;
+    private Vector2 originePointVector=Vector2.zero;
+
+    private bool jumpRelaodİsTrue=true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        jumpRelaodTime=jump.durationTime+jump.hangTime;
+    }
+
+    void FixedUpdate()
+    {
+        CheckKeyState();
+
+       
+    }
+
+    private void CheckKeyState()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow)&&jumpRelaodİsTrue)
+        {
+            Jump(upDirection);
+
+        }
+        
+        else if (Input.GetKeyDown(KeyCode.DownArrow)&&jumpRelaodİsTrue)
+        {
+            Jump(downDirection);
+
+        }
+        else if(Input.GetKeyDown(KeyCode.RightArrow)&&jumpRelaodİsTrue)
+        {
+            Jump(rightDirection);
+
+
+        }
+        else if(Input.GetKeyDown(KeyCode.LeftArrow)&&jumpRelaodİsTrue)
+        {
+            Jump(leftDirection);
+
+        }
+
+    }
+
+
+    private void Jump(Vector2 newDirection)
+    {
+        OnJump();
+        rb.velocity=newDirection*jump.speed;
+        OffJumpAfterTime();
+
+    }
+    private void StopJump()
+    {
+        rb.velocity=originePointVector;
+    }
+
+
+
+
+    private void OnJump()
+    {
+        jumpRelaodİsTrue=false;
+       
+    }
+    private void OffJump()
+    {
+        jumpRelaodİsTrue=true;
+        
+
+    }
+    private void OffJumpAfterTime()
+    {
+        
+        Invoke("StopJump",jump.durationTime);
+        Invoke("OffJump",(jumpRelaodTime));
+        
+    }
+
+}
